7b06962db5cd153678f6133f3dea65f5b3b77f83
pcast11 <pcast11@vt.edu>
2018-11-29 18:06:34 -0500

    Added more VR stuff in basement

---
+++ Assets/SteamVR_Input/ViveInput.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ViveInput : MonoBehaviour {
+
+    [SteamVR_DefaultAction("InteractUI")]
+
+    public SteamVR_Action_Boolean clickAction;
+    
+
+    public SteamVR_Action_Boolean touchpadAction;
+
+	// Update is called once per frame
+	void Update () {
+        if (SteamVR_Input._default.inActions.Teleport.GetStateDown(SteamVR_Input_Sources.Any))
+        {
+            print("Teleport down");
+        }
+
+        if (SteamVR_Input._default.inActions.InteractUI.GetStateDown(SteamVR_Input_Sources.Any))
+        {
+            print("Interact down");
+        }
+
+        bool triggerValue = clickAction.GetChanged(SteamVR_Input_Sources.Any);
+
+        if (triggerValue)
+        {
+            print(triggerValue);
+        }
+
+        bool touchpadValue = touchpadAction.GetChanged(SteamVR_Input_Sources.Any);
+
+        if (touchpadValue)
+        {
+            print(touchpadValue);
+        }
+
+        /*
+        Vector2 touchpadValue = touchpadAction.GetAxis(SteamVR_Input_Sources.Any);
+
+        if(touchpadValue != Vector2.zero)
+        {
+            print(touchpadValue);
+        }
+        */
+    }
+}
